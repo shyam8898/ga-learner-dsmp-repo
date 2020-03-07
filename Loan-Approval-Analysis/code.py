a09eb6c126772fe2461cdf0ffd894dfb81aea459
shyam8898 <shyam8898@users.noreply.github.com>
2020-03-07 18:25:40 +0530

    Code file created.

---
+++ Loan-Approval-Analysis/code.py
@@ -0,0 +1,79 @@
+# --------------
+# Import packages
+import numpy as np
+import pandas as pd
+from scipy.stats import mode 
+ 
+
+
+
+# code starts here
+bank=pd.read_csv(path)
+categorical_var=bank.select_dtypes(include = 'object')
+print(categorical_var)
+numerical_var=bank.select_dtypes(include = 'number')
+print(numerical_var)
+
+# code ends here
+
+
+# --------------
+# code starts here
+banks=bank.drop(columns='Loan_ID')
+bank_mode=banks.mode().iloc[0]
+banks.fillna(bank_mode,inplace=True)
+#code ends here
+
+
+# --------------
+# Code starts here
+
+
+
+
+
+
+
+avg_loan_amount=banks.pivot_table(index=['Gender','Married','Self_Employed'],values='LoanAmount')
+
+
+
+# code ends here
+
+
+
+# --------------
+# code starts here
+
+
+
+
+
+loan_approved_se=len(banks[(banks['Self_Employed']=='Yes') & (banks['Loan_Status']=='Y')])
+loan_approved_nse=len(banks[(banks['Self_Employed']=='No') & (banks['Loan_Status']=='Y')])
+percentage_se = (loan_approved_se * 100 / 614)
+percentage_nse = (loan_approved_nse * 100 / 614)
+percentage_nse
+# code ends here
+
+
+# --------------
+# code starts here
+loan_term=banks['Loan_Amount_Term'].apply(lambda x:int(x)/12)
+big_loan_term=len(loan_term[loan_term >= 25])
+big_loan_term
+
+
+# code ends here
+
+
+# --------------
+# code starts here
+loan_groupby=banks.groupby('Loan_Status')
+loan_groupby=loan_groupby[['ApplicantIncome','Credit_History']]
+mean_values=loan_groupby.mean()
+
+
+# code ends here
+
+
